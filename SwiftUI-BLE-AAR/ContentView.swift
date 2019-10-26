9ba573bb189983a1c2f89ec2f148f9ec6b7a29b3
Paul Kang <ios_dev@paul-kang.com>
2019-10-26 11:06:08 +0800

    Initial Commit

---
+++ SwiftUI-BLE-AAR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-BLE-AAR
+//
+//  Created by paulchainkang on 2019/10/26.
+//  Copyright © 2019 paulchainkang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
